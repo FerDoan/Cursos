e96d7741f5796ffe0e97bfa672225a834fd530bf
FerDoan <fpuentes@ari.group>
2023-02-23 13:58:12 -0600

    task: pass data js in table html

--- ZRefuerzo/js/data.js
+++ ZRefuerzo/js/data.js
@@ -1,13 +1,29 @@
 const datos = {
     usuarios:[
         {
+            id:1,
             name:'Fer',
-            password:'1234'
+            password:'1234',
+            admin:'1'
         },
         {
+            id:2,
             name:'Armando',
-            password:'1234'
-        }
+            password:'1234',
+            admin:'1'
+        },
+        {
+            id:3,
+            name:'Brandon',
+            password:'4321',
+            admin:'0'
+        },
+        {
+            id:4,
+            name:'Itzel',
+            password:'4321',
+            admin:'1'
+        },
     ]
 };
 
--- ZRefuerzo/js/index.js
+++ ZRefuerzo/js/index.js
@@ -1,5 +1,5 @@
-// import nuser from "./login.js";
-// import { gName } from "./dataGlobal.js";
+import data from '../js/data.js'
+
 document.getElementById('user').textContent = 'Fer';
 
 // console.log(gName)
@@ -17,7 +17,7 @@ const sideHideC = () => {
     // sideHC.classList.add('hide')
     // sideHC.classList.remove('show')
     sideHC.classList.toggle('side-barO')
-
+    console.log(data)
     liO.forEach((li) => {
         li.classList.toggle('liO')
     })
@@ -41,4 +41,60 @@ const sideHideA = () => {
 
 btnSideC.addEventListener('click', sideHideC)
 // btnSideA.addEventListener('click', sideHideA)
+// variable de esta tabla}
+// const datos = data.usuarios;
+// Crear datos en tabla dinamicamente
+const ttlb = document.getElementById('tlb');
 
+data.usuarios.map((user) =>{
+    console.log(user)
+    // const tlb = document.createElement('table')
+    const trUsers = document.createElement('tr');
+    const dtName = document.createElement('td');
+    const dtPass = document.createElement('td');
+    const dtAccion = document.createElement('td');
+    dtAccion.style.textAlign = 'center';
+    let txtPass = document.createTextNode(user.password);
+    let txtName = document.createTextNode(user.name);
+    //Creacion de botones
+    const check = document.createElement('input');
+    check.id = user.name;
+    check.type = 'checkbox';
+
+    if (user.admin == '1') {
+        check.checked = true;
+    }
+
+    dtAccion.appendChild(check)
+    dtName.appendChild(txtName);
+    dtPass.appendChild(txtPass);
+    trUsers.appendChild(dtName);
+    trUsers.appendChild(dtPass);
+    trUsers.appendChild(dtAccion);
+    ttlb.appendChild(trUsers);
+    // tlb.appendChild(ttlb);
+    
+    const checkU = document.getElementById(user.name);
+    // if (user.admin == '1') {
+    //     checkU.checked = true
+    //     console.log('activo')
+    // }
+    const valorChec = (e) =>{
+        // e.preventDefault();
+        // dtAccion.checked 
+        // console.log(checkU)
+        // if (user.admin == '1') {
+        //     dtAccion.checked
+        // }       
+        if (checkU.checked) {
+            user.admin = '1';
+
+            // console.log(user.id)
+        }else{
+            user.admin = '0';
+        }
+    }
+     checkU.addEventListener('click',valorChec)
+    
+
+})
